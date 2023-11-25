198ef99d9c5bba9615a965f435892fd0346cf55e
Rodrigo Padilha <151454973+rodrigoPadi@users.noreply.github.com>
2023-11-24 22:06:35 -0300

    Create 1009 - Salary with Bonus.py

---
+++ 1009 - Salary with Bonus.py
@@ -0,0 +1,6 @@
+name = str(input())
+fixed_salary = float(input())
+total_sales = float(input())
+total_salary = (total_sales * 0.15) + fixed_salary
+
+print('TOTAL = R$ {:.2f}'.format(total_salary))
