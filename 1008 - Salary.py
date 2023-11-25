b280b670da69ac5f1bdb61272188b85e8968c0f2
Rodrigo Padilha <151454973+rodrigoPadi@users.noreply.github.com>
2023-11-24 21:40:45 -0300

    Create 1008 - Salary.py

---
+++ 1008 - Salary.py
@@ -0,0 +1,7 @@
+employeer_number = int(input())
+hours_number = int(input())
+amount_per_hour = float(input())
+salary = hours_number * amount_per_hour
+
+print('NUMBER = {}'.format(employeer_number))
+print('SALARY = U$ {:.2f}'.format(salary))
